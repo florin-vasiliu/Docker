2d63e5c7c38e32469f1d61b9256dc4889993799b
Florin Vasiliu <favasiliu@gmail.com>
2020-08-30 20:47:19 -0500

    finished RESTful Routes

--- project/api/users.py
+++ project/api/users.py
@@ -22,6 +22,10 @@ user = api.model('User', {
 
 class UsersList(Resource):
 
+    @api.marshal_with(user, as_list=True)
+    def get(self):
+        return User.query.all(), 200
+
     @api.expect(user, validate=True) # new
     def post(self):
         post_data = request.get_json()
